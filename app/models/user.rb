b001f4498a4ec3300b180a72a3de1b3d8fc0bc0b
Sagar <sagar.patil.smp@gmail.com>
2013-04-12 20:02:10 +0530

    user profile image

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,9 +7,9 @@ class User < ActiveRecord::Base
    mount_uploader :image, AvatarUploader
    def self.create_with_omniauth(auth)
     #binding.pry
-    #temp = Tempfile.new([auth["uid"], ".jpg"])
-    #temp.binmode
-    #temp.write(open(auth["info"]["image"]).read)
-    self.create(:provider=>auth["provider"],:uid=>auth["uid"],:name=>auth["info"]["name"],:image => auth["info"]["image"])
+    temp = Tempfile.new([auth["uid"], ".jpg"])
+    temp.binmode
+    temp.write(open(auth["info"]["image"]).read)
+    self.create(:provider=>auth["provider"],:uid=>auth["uid"],:name=>auth["info"]["name"],:image => temp)
   end
 end
